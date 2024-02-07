17943d4b06d44e4abbb18c8a40a7ec014178c73f
Ched2370 <mmibz.85@gmail.com>
2024-02-07 20:35:10 -0300

    eliminando parseo en ejercicio 6, ya que no era necesario

--- ejercicio 6/app.js
+++ ejercicio 6/app.js
@@ -1,7 +1,7 @@
 try {
     var x = [32, 45, 78, 99, 12, 21];
     for (let y of x) {
-        if(Number(y) % 3 == 0){
+        if(y % 3 == 0){
             console.log(y);
             document.write(y + '<br>');
         }
